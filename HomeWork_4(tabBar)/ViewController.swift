3a53ed315356f30b26ab8c074fa764375a80487c
Pavel <>
2022-04-08 17:46:03 +0500

    1
    1

--- HomeWork_4(tabBar)/ViewController.swift
+++ HomeWork_4(tabBar)/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("master")
+        print("master")
+        print("master")
+        print("master")
+        print("master")
+        print("master")
+        print("master")
+        print("master")
+        print("master")
+        print("master")
+        print("master")
+
     }
 
 
